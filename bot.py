d403244fd8d12f891681ec851ccf4f723b0c0aa7
gombb <primegombb@protonmail.com>
2021-02-07 15:00:15 +0100

    fix: typo from line 241

--- bot.py
+++ bot.py
@@ -238,7 +238,7 @@ def error(e: 'BinanceApiException'):
 
 user_session = {"in_position": False}
 user_session["balance"] = req_user_data.request_user_balance(ASSET_TICKER)["balance"]
-sync_session_positon(SYMBOL)200
+sync_session_positon(SYMBOL)
 _5_min_close = []
 _15_min_close = []
 
