e0484af640388eaa3871509ccf7c458a14c74406
gombb <primegombb@protonmail.com>
2021-02-26 23:51:55 +0100

    refactor: removed deadcode from strat.py

--- strat.py
+++ strat.py
@@ -73,11 +73,3 @@ def test_TP(rsi):
     if rsi[-1] > 25:
         return True 
 
-# tick_price = 32500
-# rsi = [38]
-# sma21 = [32427, 32436]
-# highTFema=[32555, 32666]
-
-# print(sma21_bull_sell([80.25]))
-# print(sma21_bull_buy(tick_price, rsi, sma21, highTFema))
-# print(sma21_bear_sell(tick_price, rsi, sma21, highTFema))
