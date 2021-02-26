3c2386a021d1da7a671545ab826d782d6a28030d
gombb <primegombb@protonmail.com>
2021-02-26 23:51:10 +0100

    refactor: generalized variables in bull/bear strat, add separate bull/bear trend functions

--- strat.py
+++ strat.py
@@ -18,13 +18,23 @@ def sma21_bull_backcross(sma_5min, ema_15min):
 
 
 
-def sma21_bull_buy(tick_price, rsi, sma21, highTFema):    
+def bull_trend(low_moving_average, high_moving_average):
+    return low_moving_average[-2] > high_moving_average[-2]
+
+
+
+def bear_trend(low_moving_average, high_moving_average):
+    return high_moving_average[-2] > low_moving_average[-2]
+
+
+
+def sma21_bull_buy(tick_price, rsi, low_TF_MA, high_TF_MA):    
     rsi_is_bull = rsi[-1] >= 51
-    low_crossed_sma = tick_price <= sma21[-1]
-    bulltrend = sma21[-2] > highTFema[-2]
-    # print(f'rsi is bull: {rsi_is_bull}')
-    # print(f'low crossed sma: {price_crossed_sma}')
-    # print(f'bulltrend: {bulltrend}')
+    low_crossed_sma = tick_price <= low_TF_MA[-1]
+    bulltrend = low_TF_MA > high_TF_MA
+    print(f'rsi is bull: {rsi_is_bull}')
+    print(f'low crossed sma: {low_crossed_sma}')
+    print(f'bulltrend: {bulltrend}')
     result = {"rsi_is_bull": rsi_is_bull, "low_crossed_sma": low_crossed_sma, "bulltrend": bulltrend}    
     if rsi_is_bull and low_crossed_sma and bulltrend:
         return True
@@ -39,14 +49,14 @@ def sma21_bull_sell(rsi):
         return False
 
 
-def sma21_bear_sell(tick_price, rsi, sma21, highTFema):
+def sma21_bear_sell(tick_price, rsi, low_TF_MA, high_TF_MA):
     rsi_is_bear = rsi[-1] <= 49
-    high_crossed_sma = tick_price >= sma21[-1]
-    beartrend = sma21[-2] < highTFema[-2]
-    # print("________")
-    # print(f'rsi is bear: {rsi_is_bear}')
-    # print(f'high crossed sma: {price_crossed_sma}')
-    # print(f'beartrend: {beartrend}')
+    high_crossed_sma = tick_price >= low_TF_MA[-1]
+    beartrend = high_TF_MA > low_TF_MA
+    print("________")
+    print(f'rsi is bear: {rsi_is_bear}')
+    print(f'high crossed sma: {high_crossed_sma}')
+    print(f'beartrend: {beartrend}')
     result = {"rsi_is_bear": rsi_is_bear, "high_crossed_sma": high_crossed_sma, "beartrend": beartrend}
     if rsi_is_bear and high_crossed_sma and beartrend:
         return True
