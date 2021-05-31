5ba058d774cd60d7061ea3bc6179ca338fd3670a
Gombb <primegombb@protonmail.com>
2021-05-31 15:42:19 +0200

    feat: update compile script with -y tag

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get install build-essential
+sudo apt -y dist-upgrade
+sudo apt -y install build-essential
 sudo apt-get update
-sudo apt install python3-pip
-sudo apt-get install screen
+sudo apt -y install python3-pip
+sudo apt -y install screen
 
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzf ta-lib-0.4.0-src.tar.gz
